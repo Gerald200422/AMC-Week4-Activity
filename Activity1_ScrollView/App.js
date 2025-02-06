b1d061032c4bcfdfb447053c96a5aea35b80b5fb
Gerald P. Balete <geraldbalete123@gmail.com>
2025-02-06 22:23:59 +0800

    Activity1 "ScrollView"

---
+++ Activity1_ScrollView/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+const App = () => (
+<ScrollView>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+<Text style={{fontSize: 96}}>This is scroll view</Text>
+</ScrollView>
+);
+
+export default App;
